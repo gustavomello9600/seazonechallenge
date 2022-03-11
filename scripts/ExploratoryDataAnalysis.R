32668b6e268a864499f6adce974fcd5e9ede3923
gustavomello9600 <gustavomelloenciv@gmail.com>
2022-03-11 15:01:32 -0300

    eda: adds code for EDA plots

--- scripts/ExploratoryDataAnalysis.R
+++ scripts/ExploratoryDataAnalysis.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+library(tidyverse, lubridate)
 
 # Loads datasets as tibbles
 cwd <- getwd()
@@ -27,12 +27,37 @@ tidy.listings <- listings %>%
   extract(Categoria, c("Categoria", "Quartos"), "[HOU]*([A-Z]+)([0-9])*Q*",
           convert=TRUE) %>%
   mutate(Categoria=as.factor(Categoria)) %>%
-  select(-c("Hotel")) %>%
+  select(-c("Hotel", "Endereço")) %>%
   mutate(Categoria=fct_collapse(Categoria, TOP=c("TOP", "TOPM")))
 
 tidy.daily.revenue <- daily.revenue %>%
   select(-c("occupancy", "blocked", "revenue")) %>%
   mutate(across(-c("listing"), as.character)) %>%
   mutate(last_offered_price=parse_double_with_comma(last_offered_price)) %>%
-  mutate(across(contains("date"), function(x){as.Date(parse_datetime(x))})) %>%
+  mutate(across(contains("date"), function(x){as_date(parse_datetime(x))})) %>%
   mutate(reservation_advance=date - creation_date)
+
+listings.daily.revenue <- tidy.daily.revenue %>%
+  filter(last_offered_price > 0) %>%
+  left_join(tidy.listings, by=c("listing" = "Código")) %>%
+  mutate(revenue=last_offered_price*Comissão) %>%
+  mutate(across(Travesseiros, ~ifelse(is.na(.),
+                                      2*(Cama.Casal + Cama.Queen + Cama.King)
+                                      + 1*(Cama.Solteiro + Sofá.Cama.Solteiro),
+                                      .)))
+
+# Plots total revenue across time
+listings.daily.revenue %>%
+  select(date, revenue) %>%
+  group_by(date) %>%
+  summarise(revenue=sum(revenue)) %>%
+  ggplot(aes(x=date, y=revenue)) +
+  geom_point() +
+  geom_smooth()
+
+# Investigates the interval in which prices are accessible
+tidy.daily.revenue %>%
+  ggplot(aes(x=date, y=last_offered_price)) +
+  geom_point()
+
+
