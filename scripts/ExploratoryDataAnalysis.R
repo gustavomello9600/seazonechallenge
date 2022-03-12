9fcdc5cdf074b576293d2716b55d55b2537fa7cd
gustavomello9600 <gustavomelloenciv@gmail.com>
2022-03-11 22:34:33 -0300

    model: Fits multivariate linear regression model on part of the available data
    * One model for general use and another one for hotel rooms
    * Both predict last offered price on a day from relevant regressors

--- scripts/ExploratoryDataAnalysis.R
+++ scripts/ExploratoryDataAnalysis.R
@@ -67,3 +67,108 @@ listings.daily.revenue %>%
   summarise(mean_price=mean(last_offered_price), mean_revenue=mean(revenue))
 # mean_price: 535; mean_revenue:107
 
+# Answers second challenge question
+library(caret)
+
+price.estimation.data <- listings.daily.revenue %>%
+  filter(last_offered_price > 0) %>%
+  group_by(Hotel) %>%
+  group_split(.keep = FALSE)
+
+price.estimation.data.not.hotel <- price.estimation.data[[1]]
+price.estimation.data.hotel <- price.estimation.data[[2]]
+
+## Model for estimating prices for apartment and houses
+price.estimation.data.not.hotel <- price.estimation.data.not.hotel %>%
+  mutate(weekday=relevel(as.factor(as.character(wday(date, label = TRUE))),
+                         "seg"),
+         Localização=relevel(Localização, "CENTRO"),
+         Categoria=relevel(Categoria, "SIM"))%>%
+  select(last_offered_price, weekday, reservation_advance, Localização,
+         Categoria, Quartos, Cama.Casal, Cama.Solteiro, Cama.Queen,
+         Cama.King, Sofá.Cama.Solteiro, Taxa.de.Limpeza, Banheiros, Capacidade)
+
+### Splits the data in training and testing sets
+set.seed(1)
+train.index <- createDataPartition(price.estimation.data.not.hotel$last_offered_price,
+                                   p=0.60, list=FALSE)
+training <- price.estimation.data.not.hotel[train.index,]
+testing <- price.estimation.data.not.hotel[-train.index,]
+
+### Trains a multivariate linear regression model optimally
+first.model <- lm(last_offered_price ~ ., training)
+model.no.hotel <- step(first.model)
+
+### Assesses model fit on testing and training data
+summary(model.no.hotel) # R² = 93.58%
+
+predictions <- predict(model.no.hotel, testing)
+RMSE.on.training.set <- sqrt(mean(model.no.hotel$residuals^2))
+RMSE.on.testing.set <- sqrt(mean((predictions - testing$last_offered_price)^2))
+
+c("Training RMSE"=RMSE.on.training.set, #91.00553
+  "Testing RMSE"=RMSE.on.testing.set)   #99.38460
+
+## Model for estimating prices for hotel rooms
+price.estimation.data.hotel <- price.estimation.data.hotel %>%
+  mutate(weekday=relevel(as.factor(as.character(wday(date, label = TRUE))),
+                         "seg"),
+         Localização=relevel(Localização, "CENTRO"),
+         Categoria=relevel(Categoria, "SIM"))%>%
+  select(last_offered_price, weekday, reservation_advance, Localização,
+         Categoria, Cama.Casal, Cama.Solteiro, Cama.Queen,
+         Cama.King, Sofá.Cama.Solteiro, Banheiros, Capacidade)
+
+### Splits the data in training and testing sets
+set.seed(1)
+train.index <- createDataPartition(price.estimation.data.hotel$last_offered_price,
+                                   p=0.70, list=FALSE)
+training <- price.estimation.data.hotel[train.index,]
+testing <- price.estimation.data.hotel[-train.index,]
+
+### Trains a multivariate linear regression model optimally
+first.model <- lm(last_offered_price ~ ., training)
+model.hotel <- step(first.model)
+
+### Assesses model fit on testing and training data
+summary(model.hotel) # R² = 61.38%
+
+predictions <- predict(model.hotel, testing)
+RMSE.on.training.set <- sqrt(mean(model.hotel$residuals^2))
+RMSE.on.testing.set <- sqrt(mean((predictions - testing$last_offered_price)^2))
+
+c("Training RMSE"=RMSE.on.training.set, #114.6488
+  "Testing RMSE"=RMSE.on.testing.set)   #116.4826
+
+## Generalizes for the whole data
+price.data <- listings.daily.revenue %>%
+  mutate(weekday=as.factor(as.character(wday(date, label = TRUE)))) %>%
+  group_by(Hotel) %>%
+  group_split(.keep = FALSE)
+
+### Uses both models to make predictions, then rejoins the dataset
+price.data.no.hotel <- price.data[[1]]
+price.data.hotel <- price.data[[2]]
+
+predictions.no.hotel <- predict(model.no.hotel, price.data.no.hotel %>%
+                                  mutate(Localização=fct_collapse(Localização, CAN=setdiff(
+                                    levels(price.data.no.hotel$Localização),
+                                    model.no.hotel$xlevels$Localização))))
+
+predictions.hotel <- predict(model.hotel, price.data.hotel%>%
+                               mutate(Localização=fct_collapse(Localização, ILC=setdiff(
+                                 levels(price.data.hotel$Localização),
+                                 model.hotel$xlevels$Localização))))
+
+price.data.no.hotel <- price.data.no.hotel %>%
+  mutate(predicted.price=predictions.no.hotel)
+price.data.hotel <- price.data.hotel %>%
+  mutate(predicted.price=predictions.hotel)
+
+price.data <- full_join(price.data.no.hotel, price.data.hotel)
+
+# Exploratory Analysis on Predicted Price Data
+p <- price.data %>%
+  group_by(date) %>%
+  summarise(predicted.revenue=sum(predicted.price*Comissão))
+
